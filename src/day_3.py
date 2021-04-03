3b4350de15be41b952fa9a3fdf2f507f9a6108db
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-04-04 03:15:05 +1200

    adjust day 3 for part 2 while keeping functionality

--- src/day_3.py
+++ src/day_3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.9
 
+from functools import partial
+
 
 class CircList(list):
     def __getitem__(self, i):
@@ -21,16 +23,18 @@ def get_data() -> list[CircList[bool]]:
         return list(map(parse_row, rows))
 
 
-def find_hit(angle: int, rows: list[CircList[bool]], index: int = 0) -> int:
+def find_hit(rows: list[CircList[bool]], right: int, down: int, index: int = 0) -> int:
     if not rows:
         return 0
-    x, *xs = rows
-    return x[index] + find_hit(angle, xs, index + angle)
+
+    rows[0][index]
+    return rows[0][index] + find_hit(rows[down:], right, down, index + right)
 
 
 def main():
     vals = get_data()
-    print(find_hit(3, vals))
+    hit = partial(find_hit, vals)
+    print(hit(3, 1))
 
 
 if __name__ == "__main__":
