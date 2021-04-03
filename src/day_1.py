d4f04caab8dab6717180f859697cf0319dbf4d1d
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-04-03 19:37:30 +1300

    do day 1 part 1

--- src/day_1.py
+++ src/day_1.py
@@ -1,11 +1,20 @@
-def get_data():
+def get_data() -> list[int]:
     with open("../data/day_1_input.txt") as f:
         rows = f.readlines()
         return list(map(int, rows))
 
 
+def find_sum(total: int, nums: set[int]) -> tuple[int, int]:
+    for i in nums:
+        if (j := (total - i)) in nums:
+            return (i, j)
+    raise ValueError
+
+
 def main():
-    print(get_data())
+    vals = set(get_data())
+    x, y = find_sum(2020, vals)
+    print(x * y)
 
 
 if __name__ == "__main__":
