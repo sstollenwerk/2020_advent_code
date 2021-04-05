28fc659e74418df42487520bea6355a0dcb06f55
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-04-06 06:41:01 +1200

    do day 10 part 2
    If this were a larger program, it might have been better to create a 
    Directed Acyclic graph class/object, but simple is better than complex.

--- src/d10.py
+++ src/d10.py
@@ -2,23 +2,40 @@ from typing import Iterator
 
 import more_itertools
 
+from functools import cache
+
+
 def get_data() -> list[int]:
     with open("../data/d10.txt") as f:
         rows = f.readlines()
         return list(map(int, rows))
 
 
-def use_all_order(vals:list[int]) -> list[int]:
+@cache
+def arrangements(vals: frozenset[int], n: int = None) -> int:
+    if n is None:
+        n = max(vals)
+    if n == 0:
+        return 1
+
+    nexts = set(range(n - 3, n)) & vals
+
+    return sum((arrangements(vals, i) for i in nexts))
+
+
+def use_all_order(vals: list[int]) -> list[int]:
     return sorted(vals)
 
-def diffs(vals:list[int]) -> list[int]:
-    return [b-a for (a,b) in more_itertools.pairwise(iter(vals))]
+
+def diffs(vals: list[int]) -> list[int]:
+    return [b - a for (a, b) in more_itertools.pairwise(iter(vals))]
+
 
 def main():
-    vals = get_data() + [0] + [ max(get_data()) + 3]
+    vals = get_data() + [0] + [max(get_data()) + 3]
     diff = diffs(use_all_order(vals))
 
-    print(diff.count(1) * diff.count(3) )
+    print(arrangements(frozenset(vals)))
 
 
 if __name__ == "__main__":
